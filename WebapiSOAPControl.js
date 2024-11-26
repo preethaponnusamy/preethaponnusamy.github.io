90202b58ac08713033da872f62ba0db79ff50c77
preethaponnusamy <preethaponnusamy@gmail.com>
2024-11-26 14:05:47 -0600

    Add files via upload

--- WebapiSOAPControl.js
+++ WebapiSOAPControl.js
@@ -32,31 +32,32 @@ export class PreethaWebApiRequestSOAPDev extends LitElement {
         };
     }
 
-     constructor() {
-    super();
-    this.who = 'World';
-    this.response = '';
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    console.log("Calling SOAP endpoint - start");
-    this.makeSoapRequest();
-    console.log("Calling SOAP endpoint - end");
-  }
-
-  // Send SOAP request when the component is first updated 
-  updated(changedProperties) {
-    super.updated(changedProperties);
-    if (!changedProperties.has('response')) {
-      this.makeSoapRequest();
+    constructor() {
+        super();
+        this.who = 'World';
+        this.response = '';
+        this.jsonData=null;
     }
-  }
 
-  async makeSoapRequest() {
-    const serviceID = 'era-pmt-branchinfo-serviceid';
-    const servicePassword = '_pkJ59$=XVs.gWM7wP:539p$_$@]uX';
-    const soapEnvelope = `
+    connectedCallback() {
+        super.connectedCallback();
+        console.log("Calling SOAP endpoint - start");
+        this.makeSoapRequest();
+        console.log("Calling SOAP endpoint - end");
+    }
+
+    // Send SOAP request when the component is first updated 
+    updated(changedProperties) {
+        super.updated(changedProperties);
+        if (!changedProperties.has('response')) {
+            this.makeSoapRequest();
+        }
+    }
+
+    async makeSoapRequest() {
+        const serviceID = 'era-pmt-branchinfo-serviceid';
+        const servicePassword = '_pkJ59$=XVs.gWM7wP:539p$_$@]uX';
+        const soapEnvelope = `
       <soapenv:Envelope xmlns:erad="http://www.pnc.com/pmt/ERADBLookupService" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
         <soapenv:Header>
           <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
@@ -73,57 +74,133 @@ export class PreethaWebApiRequestSOAPDev extends LitElement {
           </erad:MarketNameRequest>
         </soapenv:Body>
       </soapenv:Envelope>`;
-       // const serviceID='ERA-PMT-PNCIHierarchyInfo-serviceID';
-       // const servicePassword='R404?d!$5dsiC3YagT+1A$M*iSUcqZ';
-       // const nonce='BwFC2HW6jfMgHr/N9OCyfw==';
-       // const createdDate='2017-12-14T21:23:28.843Z';
-       // const repIDInfo='1234';
-       //  const soapEnvelope = `<soapenv:Envelope xmlns:get="http://www.pnc.com/pmt/GetRKIInfoService" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-       //        <soapenv:Header>
-       //        <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-       //            <wsse:UsernameToken wsu:Id="UsernameToken-1">
-       //                <wsse:Username>${serviceID}</wsse:Username>
-       //                <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">${servicePassword}</wsse:Password>
-       //                <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">${nonce}</wsse:Nonce>
-       //                <wsu:Created>${createdDate}</wsu:Created>
-       //            </wsse:UsernameToken>
-       //        </wsse:Security>
-       //        </soapenv:Header>
-       //        <soapenv:Body>
-       //            <get:PNCIHierarchyInfoRequest><get:rep_id>${repIDInfo}</get:rep_id></get:PNCIHierarchyInfoRequest>
-       //         </soapenv:Body>
-       //    </soapenv:Envelope>`;
-      
-    try {
-      const response = await fetch('https://pmt-sst-qa.pncint.net/pmt-eradblookupservice', {
-        // const response = await fetch('https://pmt-sst-qa.pncint.net/pmt-getrkiinfoService', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'text/xml; charset="utf-8"',
-          'SOAPAction': 'getMarketNameList',
-            // 'SOAPAction': 'getPNCIHierarchyInfo',
-        },
-        body: soapEnvelope,
-      });
+        // const serviceID='ERA-PMT-PNCIHierarchyInfo-serviceID';
+        // const servicePassword='R404?d!$5dsiC3YagT+1A$M*iSUcqZ';
+        // const nonce='BwFC2HW6jfMgHr/N9OCyfw==';
+        // const createdDate='2017-12-14T21:23:28.843Z';
+        // const repIDInfo='1234';
+        //  const soapEnvelope = `<soapenv:Envelope xmlns:get="http://www.pnc.com/pmt/GetRKIInfoService" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+        //        <soapenv:Header>
+        //        <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+        //            <wsse:UsernameToken wsu:Id="UsernameToken-1">
+        //                <wsse:Username>${serviceID}</wsse:Username>
+        //                <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">${servicePassword}</wsse:Password>
+        //                <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">${nonce}</wsse:Nonce>
+        //                <wsu:Created>${createdDate}</wsu:Created>
+        //            </wsse:UsernameToken>
+        //        </wsse:Security>
+        //        </soapenv:Header>
+        //        <soapenv:Body>
+        //            <get:PNCIHierarchyInfoRequest><get:rep_id>${repIDInfo}</get:rep_id></get:PNCIHierarchyInfoRequest>
+        //         </soapenv:Body>
+        //    </soapenv:Envelope>`;
 
-      if (response.ok) {
-        const xml = await response.text();
-        this.response = xml;
-        // Handle the XML response here
-      } else {
-        this.response = 'Error: ' + response.statusText;
-      }
-    } catch (error) {
-      this.response = 'Error: ' + error.message;
+        try {
+            const response = await fetch('https://pmt-sst-qa.pncint.net/pmt-eradblookupservice', {
+                // const response = await fetch('https://pmt-sst-qa.pncint.net/pmt-getrkiinfoService', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/xml; charset="utf-8"',
+                    'SOAPAction': 'getMarketNameList',
+                    // 'SOAPAction': 'getPNCIHierarchyInfo',
+                },
+                body: soapEnvelope,
+            });
+
+            if (response.ok) {
+                const xml = await response.text();
+                this.response = xml;
+                var jsonData= parseXmlToJson(this.response);
+                jsonData=this.filterJson(jsonData);
+                console.log(jsonData);
+                // Handle the XML response here
+            } else {
+                this.response = 'Error: ' + response.statusText;
+            }
+        } catch (error) {
+            this.response = 'Error: ' + error.message;
+        }
     }
-  }
+    xmlToJson(xml) {
+        const obj = {};
 
-  render() {
-    return html`
+        if (xml.nodeType === 1) {
+            if (xml.attributes.length > 0) {
+                obj["@attributes"] = {};
+                for (let i = 0; i < xml.attributes.length; i++) {
+
+                    const attribute = xml.attributes.item(i);
+
+                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+
+                }
+
+            }
+
+        } else if (xml.nodeType === 3) { // Text node
+
+            obj["#text"] = xml.nodeValue.trim();
+
+        }
+
+        // Process child nodes
+
+        if (xml.hasChildNodes()) {
+
+            for (let i = 0; i < xml.childNodes.length; i++) {
+
+                const child = xml.childNodes.item(i);
+
+                const nodeName = child.nodeName;
+
+                if (!obj[nodeName]) {
+
+                    obj[nodeName] = xmlToJson(child);
+
+                } else {
+
+                    if (!Array.isArray(obj[nodeName])) {
+
+                        obj[nodeName] = [obj[nodeName]];
+
+                    }
+
+                    obj[nodeName].push(xmlToJson(child));
+
+                }
+
+            }
+
+        }
+
+        return obj;
+
+    }
+    parseXmlToJson() {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(this.xmlData, 'text/xml');
+        this.jsonData = xmlToJson(xmlDoc);
+        return this.jsonData;
+    }
+    filterJson(jsonData){     
+        if(!this.jsonPath){
+          this.jsonPath = "$."
+        }        
+        if(jsonData){         
+            var result = JSONPath({path: this.jsonPath, json: jsonData});        
+            if (result.length == 1 && this.jsonPath.endsWith(".")) {
+                result = result[0]
+              }        
+            return result;
+        }
+      }
+
+    render() {
+        return html`
       <p>Hello ${this.who},</p>
       <p>Response: ${this.response}</p>
     `;
-  }
+    }
 }
 const elementName = 'preetha-webapi-soap-request-dev';
 customElements.define(elementName, PreethaWebApiRequestSOAPDev);
