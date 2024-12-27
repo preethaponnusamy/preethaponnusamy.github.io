6540720e04e6eb04075c4515a244ff0b85782c94
preethaponnusamy <preethaponnusamy@gmail.com>
2024-12-27 15:44:44 -0800

    Add files via upload

--- OncCustomChoiceControl.js
+++ OncCustomChoiceControl.js
@@ -360,8 +360,7 @@ export class OncCustomChoiceDev extends LitElement {
       if (Array.isArray(items)) {
         let optionTemplates = [];
         for (let i of items) {
-            if(this.currentPageMode == "Edit")
-                this.outcome=JSON.parse(this.outcome);
+           
           const isSelected = this.outcome && this.outcome.includes(i);
           optionTemplates.push(html`
             <label>
@@ -397,6 +396,8 @@ export class OncCustomChoiceDev extends LitElement {
   }
 
   _handleMultiSelectChange(e) {
+    if(this.currentPageMode == "Edit")
+        this.outcome=JSON.parse(this.outcome);
     let selectedValues = [...(this.outcome || [])];
     if (e.target.checked) {
       selectedValues.push(e.target.value);
